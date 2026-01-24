5b142adef0b99a4ea7b508ada8f0a826f14d066f
Lucas Alves <olalvezluc@gmail.com>
2026-01-24 09:31:51 -0300

    Inicia server

---
+++ todo.asm
@@ -0,0 +1,15 @@
+format ELF64 executalbe 
+
+include "linux.inc"
+
+MAX_CONN        equ 5
+REQUEST_CAP     equ 128 * 1024
+TODO_SIZE       equ 256
+TODO_CAP        equ 246
+
+segment readable executable
+
+include "utils.inc"
+
+entry main
+main: 
