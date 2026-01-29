72ed02f7e70b38a9a9818539eb69b1f21807acc7
Lucas Alves <olalvezluc@gmail.com>
2026-01-28 22:54:09 -0300

    Adiciona o html do erro

--- todo.asm
+++ todo.asm
@@ -367,6 +367,13 @@ error_404            db "HTTP/1.1 404 Not found", 13, 10
                      db "<h1>Page not found</h1>", 10
                      db "<a href='/'>Back to Home</a>", 10
                      db 0
+error_405            db "HTTP/1.1 405 Method Not Allowed", 13, 10
+                     db "Content-Type: text/html; charset=utf-8", 13, 10
+                     db "Connection: close", 13, 10
+                     db 13, 10
+                     db "<h1>Method not Allowed</h1>", 10
+                     db "<a href='/'>Back to Home</a>", 10
+                     db 0
 index_page_response  db "HTTP/1.1 200 OK", 13, 10
                      db "Content-Type: text/html; charset=utf-8", 13, 10
                      db "Connection: close", 13, 10
